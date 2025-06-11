954aa7dade062316d002a119dee48ba8a00ab332
Zeeshan <ansarizeeshan521@gmail.com>
2025-06-11 15:27:37 +0530

    First Assignment done

---
+++ Assignment1/Task1.py
@@ -0,0 +1,10 @@
+num1 = input("Enter first number : ")
+num2 = input("Enter second number : ")
+
+num1 = int(num1)
+num2= int(num2)
+
+print("Addition : ", num1+num2)
+print("Subtraction : ", num1-num2)
+print("Multiplication : ", num1*num2)
+print("Division : ", num1/num2)
